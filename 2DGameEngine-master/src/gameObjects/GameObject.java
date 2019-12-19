0ade1a3a33b780db7fa0b82901079b8495589dd5
Timothy Cooper <timothy.h.cooper@gmail.com>
2019-12-19 13:05:25 -0600

    Add files via upload

---
+++ 2DGameEngine-master/src/gameObjects/GameObject.java
@@ -0,0 +1,26 @@
+package gameObjects;
+
+import java.io.File;
+import main.Vector2d;
+
+public class GameObject 
+{
+	public File tileMap;
+	public Vector2d[] occupiedTiles;
+	public Vector2d[][] textureCoords;
+	
+	public GameObject(File t, Vector2d[][] tex, Vector2d[] o)
+	{
+		tileMap = t;
+		occupiedTiles = o;
+		textureCoords = tex;
+	}
+	public GameObject(File t, Vector2d[] tex, Vector2d o)
+	{
+		tileMap = t;
+		occupiedTiles = new Vector2d[1];
+		occupiedTiles[0] = o;
+		textureCoords = new Vector2d[1][4];
+		textureCoords[0] = tex;
+	}
+}
---
+++ 2DGameEngine-master/src/gameObjects/Interactable.java
@@ -0,0 +1,7 @@
+package gameObjects;
+
+
+public interface Interactable 
+{
+	public void interact(GameObject source);
+}
